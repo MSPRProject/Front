22bcdb5416222c8b8bf85880da9e0273c71b883b
kvnmri <kvn.mri.dev@gmail.com>
2025-05-29 13:19:41 +0200

    fix

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
-
-@Component({
-  selector: 'app-sidebar',
-  imports: [CommonModule],
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
-=======
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { RouterLink } from "@angular/router";
@@ -18,7 +7,6 @@ import { RouterLink } from "@angular/router";
   imports: [CommonModule, RouterLink],
   templateUrl: "./sidebar.component.html",
   styleUrls: ["./sidebar.component.css"],
->>>>>>> 39a38d1 (feat: predict + bug fixes)
 })
 export class SidebarComponent {
   isSidebarOpen: boolean = false;
